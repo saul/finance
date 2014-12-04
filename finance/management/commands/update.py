952ecc2a72a28a9002027aaea7c4584845303c06
Saul Rennison <saul.rennison@gmail.com>
2014-12-04 10:14:27 +0000

    Install node dependencies with `manage.py update`

--- finance/management/commands/update.py
+++ finance/management/commands/update.py
@@ -5,8 +5,11 @@ from cli import console
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        console.stage_print('Migrating database...')
+        console.stage_print('Updating Python dependencies...')
         console.call(['pip', 'install', '-r', 'requirements.txt'])
+
+        console.stage_print('Updating Node dependencies...')
+        console.call(['npm', 'install'], shell=True)
 
         console.stage_print('Migrating database...')
         console.call(['python', 'manage.py', 'migrate'])
